1e2a27cf36378b671d2ef7ac696a26610242d0da
Benjamin Lee <benjamsandwich@gmail.com>
2018-05-14 01:53:49 -0700

    Hash bundle filename, inject bundle into html dynamically

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -18,4 +20,12 @@ module.exports = {
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
+  plugins: [
+    new CleanWebpackPlugin([
+      'dist',
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
 }
