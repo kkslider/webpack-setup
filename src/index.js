95e3e22e793f33a69103becdff322d3fe46c8db9
Benjamin Lee <benjamsandwich@gmail.com>
2018-05-14 02:29:33 -0700

    Add second SCSS file to confirm single outputted CSS file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/button.jsx';
 import './styles/index.scss';
+import './styles/test.scss';
 
 const title = 'Webpack setup';
 
